85f130102f64c4e793cda4d97b45d29cd3c9c5a6
김진현 <gimjinhyeon@gimjinhyeon-ui-MacBookAir.local>
2025-01-11 20:32:34 +0900

    first commit

---
+++ src/test/java/com/Omok/OmokApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Omok;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OmokApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
